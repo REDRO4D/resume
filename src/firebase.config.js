e1b4c39926e64ec570af098d7962fc8ce4da2869
Bruno Rosales <bruno.miguel_r@hotmail.com>
2021-10-02 10:59:34 -0500

    Hero

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,37 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/storage';
-import 'firebase/functions';
+import firebase from 'firebase/compat/app';// Importacion firebase/app como objeto para poderlo usar
 
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+// import 'firebase/functions';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUQAEAYBOK-POafRI1PWkuF2L69otdndw",
+    authDomain: "resume-66f13.firebaseapp.com",
+    projectId: "resume-66f13",
+    storageBucket: "resume-66f13.appspot.com",
+    messagingSenderId: "179902042035",
+    appId: "1:179902042035:web:e808af3873a4ff9a6285c6",
+    measurementId: "G-D3BZ43RH8X"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firebase();
+const storage = firebase.storage();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+// const functions = firebase.functions();
+
+export {
+    auth,
+    db,
+    storage,
+    googleAuthProvider,
+    facebookAuthProvider
+    // functions
+}
+
+export default firebaseConfig;
